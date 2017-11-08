9976dd5055fe8329df45aa038e0f4ed85e2b4265
sergi-as <chechi.as@hotmail.com>
2017-11-08 21:46:53 +0100

    suite editada

--- src/Test/Partida/Partida_Suite.java
+++ src/Test/Partida/Partida_Suite.java
@@ -1,13 +1,10 @@
-
+package Test.Partida;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 @RunWith(value = Suite.class)
 
-//si no se comenta la linea, intellij da error
-
-//@SuiteClasses(value = { Partida_Init_Test.class, Partida_Tablero_Test.class })
-@SuiteClasses(value = {})
+@SuiteClasses(value = { Partida_Init_Test.class, Partida_Tablero_Test.class,Test_Rondas.class })
 
 
 public class Partida_Suite {
