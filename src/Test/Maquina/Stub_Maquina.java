c3ab3f7dfce34e53a431d49b1cb9e0728050ecdd
tonibofarull <tonibofarull@gmail.com>
2017-11-12 11:34:03 +0100

    stub maquina

---
+++ src/Test/Maquina/Stub_Maquina.java
@@ -0,0 +1,19 @@
+package Test.Maquina;
+
+import Dominio.Dificultad;
+import Dominio.Rol;
+
+public class Stub_Maquina {
+
+    public Stub_Maquina(Stub_Normas normas, Dificultad dificultad, Rol rolJugadorHumano) {}
+
+    public String generarCandidato(String ultimoCandidato, String ultimaNB, Dificultad dificultad) {
+        return "1234";
+    }
+
+    public String generarSolucion(Dificultad dificultad) {
+        return "1234";
+    }
+
+}
+
